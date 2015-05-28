2a7825219b53a96ce6f937602e30c8486cf73087
Hill <hill@kimo.com>
2015-05-28 06:56:54 +0400

    fixed CONSTANT not found in include_plugin

--- include_plug.php
+++ include_plug.php
@@ -1,4 +1,6 @@
 <?php
+include_once('src/Constants.php');
+include_once('src/namespace.constants.php');
 include_once('src/Object.php');
 include_once('src/ListIterator.php');
 include_once('src/HashMap.php');
