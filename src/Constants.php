aec2fd1228ecddfd9750c2073fcb3ee86c809ef3
Hill <hill@kimo.com>
2017-10-30 13:23:27 +0000

    Add B4_PROCESS_HEADER life cycle

--- src/Constants.php
+++ src/Constants.php
@@ -57,6 +57,7 @@ namespace PMVC\Event {
     const B4_PROCESS_ACTION = 'B4ProcessAction';
     const B4_PROCESS_ERROR = 'B4ProcessError';
     const B4_PROCESS_MAPPING = 'B4ProcessMapping';
+    const B4_PROCESS_HEADER = 'B4ProcessHeader';
     const B4_PROCESS_VIEW = 'B4ProcessView';
     const FINISH = 'Finish';
     const MAP_REQUEST = 'MapRequest';
