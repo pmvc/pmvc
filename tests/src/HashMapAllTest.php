32aaa8e04c6eb835515f5aa119e8a4877f72e950
Hill <hill@kimo.com>
2019-08-18 10:41:07 +0000

    Add hashmapall construct test

--- tests/src/HashMapAllTest.php
+++ tests/src/HashMapAllTest.php
@@ -10,9 +10,23 @@ class HashMapAllTest extends PHPUnit_Framework_TestCase
     {
         $map = new HashMapAll([]);
         $map['foo'] = [ 'a', 'b' ];
-        $expected = new HashMapAll([
-          'foo' => new HashMapAll(['a', 'b']),
-        ]);
+        $expected = new HashMapAll(
+            [
+            'foo' => new HashMapAll(['a', 'b']),
+            ]
+        );
         $this->assertEquals($expected, $map);
     }
+
+    public function testConstruct()
+    {
+        $arr = ['foo'=>['a'], 'bar'];
+        $map = new FakeHashAll($arr);
+        $expected = new FakeHashAll(['foo'=>new FakeHashAll(['a'], true), 'bar'], true);
+        $this->assertEquals($expected, $map);
+    }
+}
+
+class FakeHashAll extends HashMapAll
+{
 }
