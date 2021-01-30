d2597a735b183549320a7128e96bcf7e7009d040
Hill <hill@kimo.com>
2021-01-30 15:15:40 +0800

    Add folders basic test

--- tests/src/UtilFolderTest.php
+++ tests/src/UtilFolderTest.php
@@ -29,6 +29,14 @@ class UtilFolderTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(1, $arr[realpath(__DIR__)]);
     }
 
+    public function testBasic()
+    {
+        $f1 = folders('fake', ["./"]);
+        $this->assertEquals(1, count($f1['folders']));
+        $f2 = folders('fake', ["../"]);
+        $this->assertEquals(2, count($f2['folders']));
+    }
+
     public function testClean()
     {
         $folders = folders('fake', [], [], true);
