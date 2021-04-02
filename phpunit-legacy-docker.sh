3f645913bf34ff845c98f7c7eadfb47f6a2ff9ee
Hill <hill@kimo.com>
2021-04-02 17:11:42 +0800

    update phpunit docker for legacy test

--- phpunit-legacy-docker.sh
+++ phpunit-legacy-docker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+VERSION=phpunit${VERSION:-5.6}
+
 case "$1" in
   bash)
-    docker-compose run --rm phpunit bash
+    docker-compose run --rm ${VERSION} bash
     ;;
 
   *)
-    docker-compose run --rm phpunit ./phpunit-legacy.sh $* 
+    docker-compose run --rm ${VERSION} ./phpunit-legacy.sh $* 
     ;;
 esac
 
