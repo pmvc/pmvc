11726c79d2578155fa3b1d429f327274cf8c32e1
Hill <hill@kimo.com>
2021-11-26 02:51:52 +0800

    fixed ci

--- tests/resources/FakeDebugDump.php
+++ tests/resources/FakeDebugDump.php
@@ -4,9 +4,10 @@ namespace PMVC;
 
 initPlugin(['debug' => null], true);
 
-class FakeDebugDump extends PlugIn implements \PMVC\PlugIn\debug\DebugDumpInterface
+class FakeDebugDump extends PlugIn implements
+    \PMVC\PlugIn\debug\DebugDumpInterface
 {
-    public function escape($s)
+    public function escape($s, $type = null)
     {
         return $s;
     }
